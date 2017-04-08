6f9fc16cff7e3d752887db99813cfd6a3afce602
my123 <mmediouni@gmx.fr>
2017-04-08 23:25:57 +0200

    Scrolling lists! Next step is synchronised toots ;)

---
+++ MastodonAPI/StatusClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MastodonAPI
+{
+    class StatusClass
+    {
+        string id;
+        string uri;
+        string url;
+        string account;
+        string in_reply_to_id;
+        string in_reply_to_account_id;
+        string reblog;
+        string content;
+
+    }
+}
