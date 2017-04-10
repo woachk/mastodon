eaf28d681d98d7b0a2435259d9219f973676b319
my123 <mmediouni@gmx.fr>
2017-04-10 22:59:27 +0200

    Adding dark theme.

--- MastodonUWA/WebContentHelper.cs
+++ MastodonUWA/WebContentHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
 namespace MastodonUWA
 {
     class WebContentHelper
@@ -19,7 +21,11 @@ namespace MastodonUWA
                               //head.Append("<meta name=\"viewport\" content=\"width=720px\">");
             head.Append("<style>");
             head.Append("html { -ms-text-size-adjust:150%;}");
-            head.Append(string.Format("h2{{font-size: 48px}} " +
+            var isDark = Application.Current.RequestedTheme == ApplicationTheme.Dark;
+
+            if (!isDark)
+            {
+                head.Append(string.Format("h2{{font-size: 48px}} " +
             "body {{background:white;color:black;font-family:'Segoe UI';font-size:12px;margin:0;padding:0;display: block;" +
             "height: 100%;" +
             "overflow-x: scroll;" +
@@ -29,6 +35,20 @@ namespace MastodonUWA
             "article{{column-fill: auto;column-gap: 80px;column-width: 500px; column-height:100%; height:630px;" +
             "}}" +
             "img,p.object,iframe {{ max-width:100%; height:auto }}"));
+            }
+            else
+            {
+                head.Append(string.Format("h2{{font-size: 48px}} " +
+            "body {{background:black;color:white;font-family:'Segoe UI';font-size:12px;margin:0;padding:0;display: block;" +
+            "height: 100%;" +
+            "overflow-x: scroll;" +
+            "position: relative;" +
+            "width: 100%;" +
+            "z-index: 0;}}" +
+            "article{{column-fill: auto;column-gap: 80px;column-width: 500px; column-height:100%; height:630px;" +
+            "}}" +
+            "img,p.object,iframe {{ max-width:100%; height:auto }}"));
+            }
             head.Append(string.Format("a {{color:blue}}"));
             head.Append("</style>");
 
