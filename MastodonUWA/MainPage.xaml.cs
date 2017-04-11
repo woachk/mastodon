23b96936bcabe9a7596d0df74b4cc6a4d115c2d4
my123 <mmediouni@gmx.fr>
2017-04-11 15:37:10 +0200

    Trying to fix by adding an ObservableCollection.

--- MastodonUWA/MainPage.xaml.cs
+++ MastodonUWA/MainPage.xaml.cs
@@ -38,6 +38,7 @@ using System.Diagnostics;
 using Windows.UI.Core;
 using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.Background;
+using System.Collections.ObjectModel;
 
 namespace MastodonUWA
 {
@@ -46,6 +47,7 @@ namespace MastodonUWA
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ObservableCollection<object> TootCollectionBind; 
         IAsyncAction tootrefresh;
         public static string getServerName()
         {
@@ -78,6 +80,8 @@ namespace MastodonUWA
             {
                 SPanel.Background = new SolidColorBrush(Windows.UI.Colors.DarkGray);
             }
+            TootCollectionBind = new ObservableCollection<object>();
+            TootContainer.DataContext = TootCollectionBind;
             base.OnNavigatedTo(e);
             var settings = (string)e.Parameter;
             AuthenticateClass token = new AuthenticateClass();
@@ -131,8 +135,9 @@ namespace MastodonUWA
                              // Insert at the beginning
                                 await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, (() =>
                                 {
+                                    var firstItem = TootContainer.Items[0];
                                     Toot toot = new Toot(status.account.acct, status.account.display_name, status.content, status.account.avatar, status.id, status.reblogged, status.favourited);
-                                    TootContainer.Items.Insert(0, toot);
+                                    TootCollectionBind.Insert(0, toot);
                                 }));
                         }
                         else if (text2[1] == " notification")
@@ -148,8 +153,8 @@ namespace MastodonUWA
                                          Toot toot;
                                          block.Text = notification.account.display_name + " favourited your post.";
                                          toot = new Toot(notification.status.account.acct, notification.status.account.display_name, notification.status.content, notification.status.account.avatar, notification.status.id, notification.status.reblogged, notification.status.favourited);
-                                         TootContainer.Items.Insert(0, block);
-                                         TootContainer.Items.Insert(0, toot);
+                                         TootCollectionBind.Insert(0, block);
+                                         TootCollectionBind.Insert(0, toot);
                                      }));
                                  }
                                  if (notification.type == "reblog")
@@ -160,8 +165,8 @@ namespace MastodonUWA
                                          Toot toot;
                                          block.Text = notification.account.display_name + " boosted your post.";
                                          toot = new Toot(notification.status.account.acct, notification.status.account.display_name, notification.status.content, notification.status.account.avatar, notification.status.id, notification.status.reblogged, notification.status.favourited);
-                                         TootContainer.Items.Insert(0, block);
-                                         TootContainer.Items.Insert(0, toot);
+                                         TootCollectionBind.Insert(0, block);
+                                         TootCollectionBind.Insert(0, toot);
                                      }));
                                  }
                                  if (notification.type == "follow")
@@ -170,7 +175,7 @@ namespace MastodonUWA
                                      {
                                          TextBlock block = new TextBlock();
                                          block.Text = notification.account.display_name + " now follows you.";
-                                         TootContainer.Items.Insert(0, block);
+                                         TootCollectionBind.Insert(0, block);
                                      }));
                                  }
                                  if (notification.type == "mention")
@@ -179,7 +184,7 @@ namespace MastodonUWA
                                      {
                                          Toot toot;
                                          toot = new Toot(notification.status.account.acct, notification.status.account.display_name, notification.status.content, notification.status.account.avatar, notification.status.id, notification.status.reblogged, notification.status.favourited);
-                                         TootContainer.Items.Insert(0, toot);
+                                         TootCollectionBind.Insert(0, toot);
                                      }));
                                  }
                              }
@@ -209,7 +214,7 @@ namespace MastodonUWA
                     if (tootlist[i].account.acct != null)
                     {
                         toot = new Toot(tootlist[i].account.acct, tootlist[i].account.display_name, tootlist[i].content, tootlist[i].account.avatar, tootlist[i].id, tootlist[i].reblogged, tootlist[i].favourited);
-                        TootContainer.Items.Add(toot);
+                        TootCollectionBind.Add(toot);
                     }
                 }
             }
@@ -226,8 +231,8 @@ namespace MastodonUWA
                             Toot toot;
                             block.Text = notifications[i].account.display_name + " favourited your post.";
                             toot = new Toot(notifications[i].status.account.acct, notifications[i].status.account.display_name, notifications[i].status.content, notifications[i].status.account.avatar, notifications[i].status.id, notifications[i].status.reblogged, notifications[i].status.favourited);
-                            TootContainer.Items.Add(block);
-                            TootContainer.Items.Add(toot);
+                            TootCollectionBind.Add(block);
+                            TootCollectionBind.Add(toot);
                         }
                         if (notifications[i].type == "reblog")
                         {
@@ -235,20 +240,20 @@ namespace MastodonUWA
                             Toot toot;
                             block.Text = notifications[i].account.display_name + " boosted your post.";
                             toot = new Toot(notifications[i].status.account.acct, notifications[i].status.account.display_name, notifications[i].status.content, notifications[i].status.account.avatar, notifications[i].status.id, notifications[i].status.reblogged, notifications[i].status.favourited);
-                            TootContainer.Items.Add(block);
-                            TootContainer.Items.Add(toot);
+                            TootCollectionBind.Add(block);
+                            TootCollectionBind.Add(toot);
                         }
                         if (notifications[i].type == "follow")
                         {
                             TextBlock block = new TextBlock();
                             block.Text = notifications[i].account.display_name + " now follows you.";
-                            TootContainer.Items.Add(block);
+                            TootCollectionBind.Add(block);
                         }
                         if (notifications[i].type == "mention")
                         {
                             Toot toot;
                             toot = new Toot(notifications[i].status.account.acct, notifications[i].status.account.display_name, notifications[i].status.content, notifications[i].status.account.avatar, notifications[i].status.id, notifications[i].status.reblogged, notifications[i].status.favourited);
-                            TootContainer.Items.Add(toot);
+                            TootCollectionBind.Add(toot);
                         }
                     }
                 }
