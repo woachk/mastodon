0165cd8807c462332d27d9b5a743c9b1486d7a26
my123 <mmediouni@gmx.fr>
2017-04-12 16:08:08 +0200

    Create the Xamarin projects.

---
+++ Mastodon.Xamarin/Mastodon.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Mastodon.Xamarin.Models;
+
+namespace Mastodon.Xamarin.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item.Text;
+			Item = item;
+		}
+
+		int quantity = 1;
+		public int Quantity
+		{
+			get { return quantity; }
+			set { SetProperty(ref quantity, value); }
+		}
+	}
+}
---
+++ Mastodon.Xamarin/Mastodon.Xamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Mastodon.Xamarin.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
