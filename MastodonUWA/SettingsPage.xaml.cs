f2db5bbbf320dcd4c76ca86c46e99fe51f1345cc
my123 <mmediouni@gmx.fr>
2017-04-13 19:04:39 +0200

    Fixing the stack panel color in dark mode.

--- MastodonUWA/SettingsPage.xaml.cs
+++ MastodonUWA/SettingsPage.xaml.cs
@@ -5,26 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 /*  Copyright (C) 2017  my123 (@never_released)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 namespace MastodonUWA
 {
     public sealed partial class SettingsPage : Page
     {
-        public SettingsPage()
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var isDark = Application.Current.RequestedTheme == ApplicationTheme.Dark;
+            if (isDark)
+            {
+                SPanel.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+        }
+            public SettingsPage()
         {
             this.InitializeComponent();
         }
